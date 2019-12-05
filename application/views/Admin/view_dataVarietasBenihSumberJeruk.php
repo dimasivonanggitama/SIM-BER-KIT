f04e744e02e20e508d8b79e7c2dbd3cd43f6aef3
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2019-12-05 12:23:33 +0700

    Fix Data Varietas Benih Sumber Jeruk highlight button text on sidebar of Admin page.

--- application/views/Admin/view_dataVarietasBenihSumberJeruk.php
+++ application/views/Admin/view_dataVarietasBenihSumberJeruk.php
@@ -44,13 +44,13 @@
 								</a>
 							</li>
 							<li>
-								<a class="sidebar-menu-active border-bottom" href="<?php echo site_url('dataKonsumen'); ?>">
-									<h6 class="font-weight-bold mdi mdi-account"> Data Konsumen</h6>
+								<a class="sidebar-menu border-bottom" href="<?php echo site_url('dataKonsumen'); ?>">
+									<h6 class="font-weight-normal mdi mdi-account"> Data Konsumen</h6>
 								</a>
 							</li>
 							<li>
-								<a class="sidebar-menu border-bottom" href="<?php echo site_url('dataVarietasBenihSumberJeruk'); ?>">
-									<h6 class="font-weight-normal mdi mdi-pokeball"> Data Varietas Benih Sumber Jeruk</h6>
+								<a class="sidebar-menu-active border-bottom" href="<?php echo site_url('dataVarietasBenihSumberJeruk'); ?>">
+									<h6 class="font-weight-bold mdi mdi-pokeball"> Data Varietas Benih Sumber Jeruk</h6>
 								</a>
 							</li>
 							<li>
